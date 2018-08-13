d6885556aae9743d6c32bb2c3a3e97e7d5a5c6a6
Tony <tony@gmail.com>
2018-08-13 20:34:30 +0800

    Change ListView to RecyclerView.

--- app/src/main/java/com/example/thunder/listviewtest/PhoneAdapter.java
+++ app/src/main/java/com/example/thunder/listviewtest/PhoneAdapter.java
@@ -1,46 +1,50 @@
 package com.example.thunder.listviewtest;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class PhoneAdapter extends ArrayAdapter<Phone>{
-    private int resourceID;
-    public PhoneAdapter(Context context, int textViewResourceId, List<Phone> objects){
-        super(context,textViewResourceId,objects);
-        resourceID=textViewResourceId;
+public class PhoneAdapter extends RecyclerView.Adapter<PhoneAdapter.ViewHolder>{
+    private List<Phone> mPhoneList;
+    public PhoneAdapter(List<Phone> phoneList){
+        mPhoneList=phoneList;
     }
-
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        Phone phone=getItem(position);//获取当前项的Phone实例
-        View view;
-        ViewHolder viewHolder;
-        if(convertView==null){
-            view= LayoutInflater.from(getContext()).inflate(resourceID,parent,false);
-            viewHolder=new ViewHolder();
-            viewHolder.phoneImage=(ImageView)view.findViewById(R.id.phone_image);
-            viewHolder.phoneName=(TextView)view.findViewById(R.id.phone_name);
-            view.setTag(viewHolder);
-        }else{
-            view=convertView;
-            viewHolder=(ViewHolder)view.getTag();
-        }
-        viewHolder.phoneImage.setImageResource(phone.getImageID());
-        viewHolder.phoneName.setText(phone.getName());
-        return view;
-    }
-    class ViewHolder{
+    static class ViewHolder extends RecyclerView.ViewHolder{
         ImageView phoneImage;
         TextView phoneName;
+        public ViewHolder(View view){
+            super(view);
+            phoneImage=(ImageView)view.findViewById(R.id.phone_image);
+            phoneName=(TextView)view.findViewById(R.id.phone_name);
+        }
+    }
+
+    //加载phone_item子布局，并创建ViewHolder获取该子布局
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.phone_item,parent,false);
+        ViewHolder holder=new ViewHolder(view);
+        return holder;
+    }
+    //为子布局赋值，在子项被滚动到屏幕内的时候执行
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Phone phone=mPhoneList.get(position);
+        holder.phoneName.setText(phone.getName());
+        holder.phoneImage.setImageResource(phone.getImageID());
+    }
+    //获取子项数目
+    @Override
+    public int getItemCount() {
+        return mPhoneList.size();
     }
 }
+
+
